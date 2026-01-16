6f0658065943e7aca435989e2e0450f12d3ebdc2
Nikita Belov <nikita2010belov@gmail.com>
2026-01-16 18:44:56 +0500

    added commands: help, exit, pkg; 'unknown command' global & for pkg
    надо бы 'about' сделать в коде в стиле как help, ибо щас как выглядит это:skull:

--- main.go
+++ main.go
@@ -24,16 +24,20 @@ func main() {
 		comm = arr[0]                       // а ета короче комманда
 		params := arr[1:]                   // а ета короче параметры её
 
-		fmt.Printf("\n---------\nfull: %s\nfirst word: %s\nparams: %v\n---------\n", fullComm, comm, params)
+		// fmt.Printf("\n---------\nfull: %s\nfirst word: %s\nparams: %v\n---------\n", fullComm, comm, params)
 
 		switch comm {
 		case "help":
-			fmt.Printf("")
+			fmt.Printf("help   display command list\n" +
+				"about  display programm version\n" +
+				"exit   closes the programm\n" +
+				"pkg [parameter] [package]   manage programm's packages\n")
+
 		case "about":
 			fmt.Printf(`..................
 ..#.....#..######.	 %s
 ..##....#....#....	 %s
-..#.#...#....#....	 By %s 2001
+..#.#...#....#....	 By %s 2026
 ..#..#..#....#....	 https://github.com/nachosteam/nachos-console
 ..#...#.#....#....
 ..#....##....#....	 %s
@@ -42,9 +46,24 @@ func main() {
 `, NAME, VER, color.YellowString("NachosTeam"), color.CyanString("Under MIT License"))
 		case "":
 			continue
+		case "exit":
+			return
+		case "pkg":
+			if len(params) >= 1 {
+				switch params[0] {
+				case "help":
+					fmt.Printf("pkg install [package]\tinstall package\n")
+				case "install":
+					fmt.Printf("pkg: install\n")
+				default:
+					fmt.Printf("pkg: Unknown parameter: \"%s\"\nEnter \"pkg help\" to see available commands\n", params[0])
+				}
+			} else {
+				fmt.Println("pkg: none")
+			}
 
 		default:
-			fmt.Printf("ээ тi чо ахуел '%s'\n", fullComm)
+			fmt.Printf("Unknown command: \"%s\"\n", comm)
 		}
 	}
 }
